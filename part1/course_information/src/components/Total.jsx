63436d22106dd05ae81c9d08b9c2c79ae08f0fa3
KayZinMaung1 <kayzinmg363@gmail.com>
2022-08-27 21:27:38 +0630

    header , content , total and part as components

---
+++ part1/course_information/src/components/Total.jsx
@@ -0,0 +1,7 @@
+const Total = (props)=>{
+    const total = props.exercises1 + props.exercises2 + props.exercises3;
+    return(
+       <p>Number of exercises {total}</p> 
+    )
+}
+export default Total;
